dac9cfc76a83cb356ef0b6eebe3e5ae9a05c2278
=nogc238 <=nmtngoc238@gmail.com>
2024-07-21 10:48:44 +0700

    update quick statistic UI panel

--- client/src/pages/PostList.js
+++ client/src/pages/PostList.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useMediaQuery, Box, LinearProgress, Typography, Grid, Card, CardContent, Chip, Avatar } from "@mui/material";
+import {
+  useMediaQuery,
+  Box,
+  LinearProgress,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  Chip,
+  Avatar,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroller";
-import { POSTS_PER_PAGE, SERVER_URL, HEADER_DROPDOWN_LIST, totalFundMapping, classificationMapping, statusMapping, statusColorMapping, statusLogoMapping } from "../constants";
+import {
+  POSTS_PER_PAGE,
+  SERVER_URL,
+  HEADER_DROPDOWN_LIST,
+  totalFundMapping,
+  classificationMapping,
+  statusMapping,
+  statusColorMapping,
+  statusLogoMapping,
+  statusColorHoverMapping,
+} from "../constants";
 import HeaderBar from "../components/Header";
 import Companion from "../components/Companion";
 import Footer from "../components/Footer";
@@ -25,19 +45,32 @@ export default function PostList() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const [filterValue, setFilterValue] = useState(() => JSON.parse(filterParams.replace(/(\w+):/g, '"$1":').replace(/:([^,}]+)/g, ':"$1"')));
+  const [filterValue, setFilterValue] = useState(() =>
+    JSON.parse(
+      filterParams.replace(/(\w+):/g, '"$1":').replace(/:([^,}]+)/g, ':"$1"')
+    )
+  );
   const [posts, setPosts] = useState(undefined);
   const [totalPosts, setTotalPosts] = useState(0);
   const [totalFilterPosts, setTotalFilterPosts] = useState(0);
-  const [classificationFilter, setClassificationFilter] = useState(filterValue.classificationFilter ? filterValue.classificationFilter : "all");
-  const [totalFundFilter, setTotalFundFilter] = useState(filterValue.totalFundFilter ? filterValue.totalFundFilter : "all");
-  const [statusFilter, setStatusFilter] = useState(filterValue.statusFilter ? filterValue.statusFilter : "all");
+  const [classificationFilter, setClassificationFilter] = useState(
+    filterValue.classificationFilter ? filterValue.classificationFilter : "all"
+  );
+  const [totalFundFilter, setTotalFundFilter] = useState(
+    filterValue.totalFundFilter ? filterValue.totalFundFilter : "all"
+  );
+  const [statusFilter, setStatusFilter] = useState(
+    filterValue.statusFilter ? filterValue.statusFilter : "all"
+  );
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const [statsData, setStatsData] = useState({});
 
-  const isProject = category.includes("du-an") || category.includes("phong-tin-hoc");
-  const title = ("Lưu trữ danh mục: " + findTitle(HEADER_DROPDOWN_LIST, "/" + category)).toUpperCase();
+  const isProject =
+    category.includes("du-an") || category.includes("phong-tin-hoc");
+  const title = (
+    "Lưu trữ danh mục: " + findTitle(HEADER_DROPDOWN_LIST, "/" + category)
+  ).toUpperCase();
   const EXCLUDED_FILTER = ["phong-tin-hoc", "wc", "loai-khac"];
 
   useEffect(() => {
@@ -53,13 +86,20 @@ export default function PostList() {
 
     const ALL = "all";
     const filterObj = {};
-    if (classificationFilter !== ALL || totalFundFilter !== ALL || statusFilter !== ALL) {
-      if (classificationFilter !== ALL) filterObj.classificationFilter = classificationFilter;
+    if (
+      classificationFilter !== ALL ||
+      totalFundFilter !== ALL ||
+      statusFilter !== ALL
+    ) {
+      if (classificationFilter !== ALL)
+        filterObj.classificationFilter = classificationFilter;
       if (totalFundFilter !== ALL) filterObj.totalFundFilter = totalFundFilter;
       if (statusFilter !== ALL) filterObj.statusFilter = statusFilter;
 
       let filterString = JSON.stringify(filterObj);
-      filterString = filterString.replace(/"(\w+)":/g, "$1:").replace(/:"([^"]+)"/g, ":$1");
+      filterString = filterString
+        .replace(/"(\w+)":/g, "$1:")
+        .replace(/:"([^"]+)"/g, ":$1");
       const queryString = "?filter=" + filterString;
       window.history.pushState({}, "", window.location.pathname + queryString);
     } else {
@@ -96,7 +136,10 @@ export default function PostList() {
       })
       .then((newPosts) => {
         setPosts([...posts, ...newPosts.data]);
-        if (newPosts.data.length === 0 || newPosts.data.length < POSTS_PER_PAGE) {
+        if (
+          newPosts.data.length === 0 ||
+          newPosts.data.length < POSTS_PER_PAGE
+        ) {
           setHasMore(false);
         }
       })
@@ -106,12 +149,22 @@ export default function PostList() {
   if (!posts || posts.length < 0) return <LoadingScreen />;
   return (
     <Box>
-      <MetaDecorator description={title} imageUrl={publicLogoUrl} />
       <HeaderBar />
 
-      <Box m={isMobile ? "24px 16px" : "88px auto"} display={"flex"} flexDirection={"column"} gap={"40px"} maxWidth={"1080px"}>
+      <Box
+        m={isMobile ? "24px 16px" : "88px auto"}
+        display={"flex"}
+        flexDirection={"column"}
+        gap={"40px"}
+        maxWidth={"1080px"}
+      >
         {title && (
-          <Typography variant="h5" fontWeight="bold" color={"#000"} textAlign={"center"}>
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            color={"#000"}
+            textAlign={"center"}
+          >
             {title}
           </Typography>
         )}
@@ -125,47 +178,118 @@ export default function PostList() {
         ) : (
           <>
             {isProject && (
-              <Grid container display={"flex"} alignItems={"center"} gap={"16px"} m={"0 auto"} bgcolor={"#f2f2f2"} p={"32px"} borderRadius={"8px"}>
-                <Box display={"flex"} flexDirection={"column"} textAlign={"center"} alignItems={"center"} gap={"16px"} m={"0 auto"}>
-                  <Typography variant="h5" fontWeight="bold" color={"red"}>
+              <Grid
+                container
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                gap={"16px"}
+                m={"0 auto"}
+                p={"0 0 0 16px"}
+                borderRadius={"8px"}
+              >
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                  textAlign={"center"}
+                  alignItems={"center"}
+                  gap={"16px"}
+                  m={"0 auto"}
+                >
+                  <Typography variant="h5" fontWeight={700}>
                     Thống kê nhanh
                   </Typography>
 
-                  <Box display={"flex"} gap={"8px"}>
-                    <Typography variant="h6">Tổng dự án trong năm:</Typography>
-                    <Typography variant="h5" fontWeight="bold" color={"red"}>
+                  <Box bgcolor={"#FFF1F0"} p={6} borderRadius={2}>
+                    <Typography variant="h3" fontWeight="bold" color={"red"}>
                       <CountUp start={0} end={totalPosts} duration={10} />
+                    </Typography>
+                    <Typography fontSize={"20px"} fontWeight={600} variant="h4">
+                      Tổng dự án trong năm:
                     </Typography>
                   </Box>
                 </Box>
 
-                <Grid container item display={"flex"} flexWrap={"wrap"} spacing={2} width={"100%"}>
+                <Grid
+                  container
+                  item
+                  spacing={2}
+                  sx={{
+                    border: "1px solid #ffffff",
+                    paddingBottom: 2,
+                    borderRadius: 2,
+                    marginTop: 2,
+                    boxShadow: 2,
+                    width: "100%",
+                    display: "flex",
+                  }}
+                >
                   {Object.entries(classificationMapping)
                     .filter(([v, l]) => !EXCLUDED_FILTER.includes(v))
-                    .map(([value, label]) => (
-                      <Grid item xs={12} md={3} sm={6}>
-                        <Card key={value} sx={{ paddingBottom: 0 }}>
-                          <CardContent>
-                            <Typography variant="body1" textAlign={"center"}>
-                              {label}: {statsData[value]?.count ?? 0}
-                            </Typography>
+                    .map(([value, label], index) => (
+                      <Grid
+                        item
+                        md={3}
+                        sm={6}
+                        xs={6}
+                        paddingTop={0}
+                        paddingRight={2}
+                        marginTop={2}
+                        borderRight={
+                          index === 3 || (isMobile && index === 1)
+                            ? ""
+                            : "4px solid #D9D9D9"
+                        }
+                      >
+                        <div>
+                          <Typography
+                            variant="h5"
+                            fontWeight={600}
+                            textAlign={"center"}
+                          >
+                            {statsData[value]?.count ?? 0}
+                          </Typography>
+                          <Typography variant="body1" textAlign={"center"}>
+                            {label}
+                          </Typography>
+                        </div>
 
-                            <Box style={{ display: "flex", gap: "8px", justifyContent: "center", marginTop: "8px" }}>
-                              {Object.keys(statusMapping).map((status) => (
-                                <Chip
-                                  variant="outline"
-                                  avatar={<img src={statusLogoMapping[status]} alt="logo" />}
-                                  label={statsData[value]?.[status] ?? 0}
-                                  sx={{ backgroundColor: statusColorMapping[status] }}
-                                  onClick={() => {
-                                    setClassificationFilter(value);
-                                    setStatusFilter(status);
-                                  }}
+                        <Box
+                          style={{
+                            display: "flex",
+                            gap: "8px",
+                            justifyContent: "center",
+                            marginTop: "8px",
+                          }}
+                        >
+                          {Object.keys(statusMapping).map((status) => (
+                            <Chip
+                              variant="outline"
+                              avatar={
+                                <img
+                                  src={statusLogoMapping[status]}
+                                  alt="logo"
                                 />
-                              ))}
-                            </Box>
-                          </CardContent>
-                        </Card>
+                              }
+                              label={statsData[value]?.[status] ?? 0}
+                              sx={{
+                                backgroundColor: statusColorMapping[status],
+                                "& .MuiChip-avatar": {
+                                  width: "16px",
+                                  height: "16px",
+                                },
+                                "&:hover": {
+                                  backgroundColor:
+                                    statusColorHoverMapping[status],
+                                },
+                              }}
+                              onClick={() => {
+                                setClassificationFilter(value);
+                                setStatusFilter(status);
+                              }}
+                            />
+                          ))}
+                        </Box>
                       </Grid>
                     ))}
                 </Grid>
@@ -174,7 +298,13 @@ export default function PostList() {
 
             {isProject && totalPosts > POSTS_PER_PAGE && (
               <>
-                <Box display={"flex"} flexDirection={isMobile ? "column" : "row"} justifyContent={isMobile ? "center" : "flex-end"} alignItems={"center"} gap={"16px"}>
+                <Box
+                  display={"flex"}
+                  flexDirection={isMobile ? "column" : "row"}
+                  justifyContent={isMobile ? "center" : "flex-end"}
+                  alignItems={"center"}
+                  gap={"16px"}
+                >
                   <StyledSelectComponent
                     label="Loại dự án"
                     inputWidth={200}
@@ -206,10 +336,12 @@ export default function PostList() {
                         label: "Tất cả",
                         value: "all",
                       },
-                      ...Object.entries(totalFundMapping).map(([value, label]) => ({
-                        label,
-                        value,
-                      })),
+                      ...Object.entries(totalFundMapping).map(
+                        ([value, label]) => ({
+                          label,
+                          value,
+                        })
+                      ),
                     ]}
                   />
 
@@ -224,10 +356,12 @@ export default function PostList() {
                         label: "Tất cả",
                         value: "all",
                       },
-                      ...Object.entries(statusMapping).map(([value, label]) => ({
-                        label,
-                        value,
-                      })),
+                      ...Object.entries(statusMapping).map(
+                        ([value, label]) => ({
+                          label,
+                          value,
+                        })
+                      ),
                     ]}
                   />
                 </Box>
@@ -238,7 +372,14 @@ export default function PostList() {
               </>
             )}
 
-            <Box maxWidth={"1080px"} width={"100%"} m={"0 auto"} display={"flex"} flexDirection={"column"} gap={"32px"}>
+            <Box
+              maxWidth={"1080px"}
+              width={"100%"}
+              m={"0 auto"}
+              display={"flex"}
+              flexDirection={"column"}
+              gap={"32px"}
+            >
               <InfiniteScroll
                 dataLength={isProject ? totalFilterPosts : posts.length}
                 hasMore={hasMore}
