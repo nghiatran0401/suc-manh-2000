cfaf3c64e18a7906f5a75891208f4f779bb60ff8
Quang Lê <quangle@Quangs-MacBook-Pro.local>
2024-08-07 20:30:33 +0700

    feat: add more test for utils

---
+++ server/__tests__/__mocks__/firebase-admin.js
@@ -0,0 +1,26 @@
+const firestoreMock = {
+  collection: jest.fn().mockReturnThis(),
+  doc: jest.fn().mockReturnThis(),
+  get: jest.fn(),
+  set: jest.fn(),
+};
+
+const authMock = {
+  // Add methods as needed
+};
+
+const bucketMock = {
+  // Add methods as needed
+};
+
+const firebaseAdminMock = {
+  firestore: jest.fn(() => firestoreMock),
+  credential: {
+    cert: jest.fn(),
+  },
+  initializeApp: jest.fn(),
+  auth: jest.fn(() => authMock),
+  storage: jest.fn(() => ({ bucket: jest.fn(() => bucketMock) })),
+};
+
+module.exports = firebaseAdminMock;
