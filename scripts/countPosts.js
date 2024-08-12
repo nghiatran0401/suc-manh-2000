7c10cbab2edaded2b5f172246ab1c03975fe2f41
lequocduyquang <duyquangbtx@gmail.com>
2024-08-12 20:36:23 +0700

    chore: add CI for script count posts

---
+++ scripts/countPosts.js
@@ -0,0 +1,19 @@
+
+const Redis = require("ioredis");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+
+async function countPosts() {
+  const redisInstance = new Redis(process.env.REDIS_PROD_URL);
+
+  try {
+    const keys = await redisInstance.keys('post:*');
+    console.log(`Total posts: ${keys.length}`);
+
+    redisInstance.disconnect();
+  } catch (error) {
+    console.error('Error counting posts:', error);
+  }
+}
+
+countPosts().catch(console.error);
