84cf8bbfe6f2fcc408e9154d67fff294195d7132
Kai <thnghia0401@gmail.com>
2024-09-29 23:01:25 +0700

    udpate indexFirestoreDocsToRedis

--- scripts/indexFirestoreDocsToRedis.js
+++ scripts/indexFirestoreDocsToRedis.js
@@ -2,6 +2,7 @@ const { firestore } = require("./firebase");
 const { convertToCleanedName } = require("../server/utils/search");
 
 const Redis = require("ioredis");
+const { removeSearchIndexAndDocuments } = require("../server/services/redis");
 const redis = new Redis(process.env.REDIS_URL);
 
 const INDEX_NAME = "post_index";
@@ -31,62 +32,68 @@ const INDEX_SCHEMA = [
   "TAG",
 ];
 
+async function createIndexAndDocuments() {
+  await redis.call("FT.CREATE", INDEX_NAME, "PREFIX", "1", "post:", ...INDEX_SCHEMA);
+
+  const collections = await firestore.listCollections();
+  for (const collection of collections) {
+    const snapshot = await collection.get();
+
+    const promises = snapshot.docs.map(async (doc) => {
+      const data = {
+        ...doc.data(),
+        collection_id: collection.id,
+        doc_id: doc.id,
+      };
+
+      await redis.call(
+        "FT.ADD",
+        INDEX_NAME,
+        `post:${data.collection_id}:${data.doc_id}`,
+        1.0,
+        "REPLACE",
+        "FIELDS",
+        "id",
+        data.id,
+        "slug",
+        data.slug,
+        "name",
+        data.name,
+        "cleanedName",
+        convertToCleanedName(data.name),
+        "publishDate",
+        data.publish_date?.toDate(),
+        "thumbnail",
+        data.thumbnailNew ? data.thumbnailNew : data.thumbnail,
+        "category",
+        data.category,
+        "classification",
+        data.classification,
+        "status",
+        data.status,
+        "totalFund",
+        data.totalFund,
+        "province",
+        data.location?.province
+      );
+    });
+
+    await Promise.all(promises);
+  }
+
+  console.log("[indexFirestoreDocsToRedis]: Succeeded!");
+  process.exit(0);
+}
+
 async function indexFirestoreDocsToRedis() {
   try {
-    // if index exists, return
+    // if index exists, remove it and create new
     await redis.call("FT.INFO", INDEX_NAME);
+    await removeSearchIndexAndDocuments();
+    await createIndexAndDocuments();
   } catch (error) {
-    // if index does not exist, create index and documents
-    await redis.call("FT.CREATE", INDEX_NAME, "PREFIX", "1", "post:", ...INDEX_SCHEMA);
-
-    const collections = await firestore.listCollections();
-    for (const collection of collections) {
-      const snapshot = await collection.get();
-
-      const promises = snapshot.docs.map(async (doc) => {
-        const data = {
-          ...doc.data(),
-          collection_id: collection.id,
-          doc_id: doc.id,
-        };
-
-        await redis.call(
-          "FT.ADD",
-          INDEX_NAME,
-          `post:${data.collection_id}:${data.doc_id}`,
-          1.0,
-          "REPLACE",
-          "FIELDS",
-          "id",
-          data.id,
-          "slug",
-          data.slug,
-          "name",
-          data.name,
-          "cleanedName",
-          convertToCleanedName(data.name),
-          "publishDate",
-          data.publish_date?.toDate(),
-          "thumbnail",
-          data.thumbnailNew ? data.thumbnailNew : data.thumbnail,
-          "category",
-          data.category,
-          "classification",
-          data.classification,
-          "status",
-          data.status,
-          "totalFund",
-          data.totalFund,
-          "province",
-          data.location?.province
-        );
-      });
-
-      await Promise.all(promises);
-    }
-
-    console.log("[indexFirestoreDocsToRedis]: Succeeded!");
-    process.exit(0);
+    // if index does not exist, create new
+    await createIndexAndDocuments();
   }
 }
 
