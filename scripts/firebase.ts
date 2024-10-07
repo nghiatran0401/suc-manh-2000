3173517e93572a126f5669d580d8fe439d72cdbf
Kai <thnghia0401@gmail.com>
2024-10-07 23:15:18 +0700

    fix firebase

--- scripts/firebase.ts
+++ scripts/firebase.ts
@@ -3,14 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const serviceAccount: ServiceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID as string,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
+  projectId: process.env.MY_FIREBASE_PROJECT_ID as string,
+  clientEmail: process.env.MY_FIREBASE_CLIENT_EMAIL as string,
+  privateKey: process.env.MY_FIREBASE_PRIVATE_KEY ? process.env.MY_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
 };
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.MY_FIREBASE_STORAGE_BUCKET,
 });
 const firestore = firebase.firestore();
 const auth = firebase.auth();
