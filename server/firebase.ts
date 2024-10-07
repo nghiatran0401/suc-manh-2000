8681953fbaf667486df4d5c2f9483dceebd4925a
Kai <thnghia0401@gmail.com>
2024-10-07 10:35:50 +0700

    fix 19

--- server/firebase.ts
+++ server/firebase.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const serviceAccount: ServiceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID as string,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") as string,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
 };
 
 firebase.initializeApp({
